09b3d78fe0f98bd763556e5139d5e22be02e4c75
funsis <s.korovin95@gmail.com>
2017-10-02 16:07:12 +0300

    Complete config gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-var gulp = require("gulp")
-var sass = require ("gulp-sass")
-var plumber = require("gulp-plumber")
-var postcss = require("gulp-postcss")
-var autoprefixer = require("autoprefixer")
+var gulp = require("gulp");
+var sass = require ("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
 
-gulp.task('style', function() {
-  gulp.src('sass/style.scss')
+gulp.task("style", function() {
+  gulp.src("sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
@@ -17,5 +18,15 @@ gulp.task('style', function() {
         "last 2 Edge versions"
       ]})
     ]))
-    .pipe(gulp.dest("css"));
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", ["style"], function(){
+  server.init({
+    server: "."
+  });
+
+  gulp.watch("sass/**/*.scss", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
 });
